bae70849a5bf641867a46ea31a0b53f878e8083f
hellt <dodin.roman@gmail.com>
2025-09-22 23:29:41 +0200

    Sync 20250922-232941

--- internal/datasource_cluster_notifier_list/cluster_notifier_list_data_source_gen.go
+++ internal/datasource_cluster_notifier_list/cluster_notifier_list_data_source_gen.go
@@ -199,7 +199,7 @@ func ClusterNotifierListDataSourceSchema(ctx context.Context) schema.Schema {
 			"kind": schema.StringAttribute{
 				Computed: true,
 			},
-			"labelselector": schema.StringAttribute{
+			"label_selector": schema.StringAttribute{
 				Optional:            true,
 				Computed:            true,
 				Description:         "a label selector string to filter the results based on CR labels",
@@ -215,7 +215,7 @@ type ClusterNotifierListModel struct {
 	Filter        types.String `tfsdk:"filter"`
 	Items         types.List   `tfsdk:"items"`
 	Kind          types.String `tfsdk:"kind"`
-	Labelselector types.String `tfsdk:"labelselector"`
+	LabelSelector types.String `tfsdk:"label_selector"`
 }
 
 var _ basetypes.ObjectTypable = ItemsType{}
